8566077dbc0cbc79e2e96c66ca8588ad2aad03ee
FractalRunner <olegcherniaiev@gmail.com>
2025-05-09 09:44:56 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+var a=parseFloat(prompt("введіть число a="))
+var b=parseFloat(prompt("введіть число b="))
+var c=parseFloat(prompt("введіть число c="))
+let z=a+b+c
+alert("a+b+c+"+a+"+"+b+"+"+c+"="+z)
